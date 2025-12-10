365c133f61e455eaa744a8e8b678239acd7d0b4d
paolo <paolo@foo.com>
2025-12-10 10:29:41 +0100

    aggiunti test ole e serial

---
+++ software/MainBlackPillCode/src/test/oled_hw.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        
+        for (;;)
+            Serial.println(F("SSD1306 allocation failed"));
+            delay(2000);
+    }
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+    display.clearDisplay();
+    display.setTextSize(1);      // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);     // Start at top-left corner
+    display.println(F("Hello, world!"));
+    display.display();
+    delay(2000);
+}
