ac925c46f6cb65e83416289dd120189ec7844fbb
paolo <paolo@foo.com>
2025-12-11 23:18:05 +0100

    aggiunto oled.test FUNZIONA!!

--- software/MainBlackPillCode/src/test/oled_hw.cpp
+++ software/MainBlackPillCode/src/test/oled_hw.cpp
@@ -1,59 +1,89 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+This example is for a 128x32 size display using I2C
+
+if you get the error:Adafruit._GFX.h not found, download Adafruit-GFX Library
+https://github.com/adafruit/Adafruit-GFX-Library
+
+
+Purchase this OLED module from Amazon: https://amzn.to/36zFvTb
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+
+* Watch the video for this code to learn it fully.
+ * Watch the video here: https://youtu.be/RjyulqVsz2o
+* this code is offered "as is" without any warranty.
+*  Updated by Ahmad Shamshiri For Robojax.com video tutorial
+* on March 18, 2018 at 10:21 at Ajax, Ontario, Canada
+* Please view other Robojax codes and videos at http://robojax.com/learn/arduino
+* if you are sharing this code, you must keep this copyright note.
+*
+*********************************************************************/
+#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
 
-#define PIN_I2C_SDA PB3
-#define PIN_I2C_SCL PB4
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_ADDR 0x3C // Common address for SSD1306
+#define OLED_I2C_ADDR 0x3C
+/*
+ * PIN connection:
+ * pin connection see: https://www.arduino.cc/en/Reference/Wire
+ * for UNO: SDA to A4, SCL to A5
+ * for Mega2560: SDA to 20, SCL to 21
+ * for Leonardo: SDA to 2, SCL to 3
+ * for Due: SDA to 20, SCL to 21
+ * VCC to 5V
+ * GND to GND :-)
+ */
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+// guarda qui: https://community.platformio.org/t/stm32f411-based-custom-pcb-i2c-debugging/48614/2
+#ifdef BLACKPILL_F411CE
+  #define PIN_WIRE_SDA PB7 
+  #define PIN_WIRE_SCL PB6
+#endif 
 
-void setup() {
+
+#define OLED_RESET -1  // Reset pin # (or -1 if sharing Arduino reset pin)
+//TODO: Adafruit considera deprecato il costruttore, convertire in nuovo
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error ("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()
+{
   Serial.begin(115200);
-  delay(2000); // Wait for Serial
 
-  // 1. CRITICAL: Initialize I2C on the specific pins BEFORE Wire.begin()
-  Wire.setSDA(PIN_I2C_SDA);
-  Wire.setSCL(PIN_I2C_SCL);
-  Wire.begin();
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  // display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
 
-  // 2. Perform an I2C scan to debug
-  Serial.println("\nScanning I2C bus...");
-  byte error, address;
-  int nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("Device found at 0x");
-      if (address<16) Serial.print("0");
-      Serial.println(address,HEX);
-      nDevices++;
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR))
+  {
+    while (1)
+    {
+      Serial.println(F("SSD1306 allocation failed"));
+      ; // Don't proceed, loop forever
     }
   }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found. Check wiring.");
-  } else {
-    Serial.println("Scan complete.");
-  }
-
-  // 3. Initialize OLED if a device was found
-  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Halt forever
-  }
-
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
-  display.println("Hello, OLED!");
-  display.display();
-  Serial.println("OLED initialized.");
+  display.setTextSize(2);              // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
 }
 
-void loop() {
-  // Your main code here
+void loop()
+{
+
+  Serial.println("OLED HW Test:ora scrivo Ciao mondo");
+  display.clearDisplay();
+  display.setCursor(0, 0); // Start at top-left corner
+
+  display.println("Ciao mondo");
+  display.println(F("1234567890"));
+ 
+  display.display();
+  delay(2000);
 }
