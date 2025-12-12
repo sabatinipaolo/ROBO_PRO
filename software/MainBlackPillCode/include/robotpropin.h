63b416a4a2926994ff8609fa65c5c88b1b374fc8
paolo <paolo@foo.com>
2025-12-12 01:09:04 +0100

    agg. test motori avanti indietro MOD: robopropin.h

--- software/MainBlackPillCode/include/robotpropin.h
+++ software/MainBlackPillCode/include/robotpropin.h
@@ -5,20 +5,23 @@
 #define PIN1_MOT_ANT_DX PB8
 #define PIN2_MOT_ANT_DX PB9
 
-#define PIN1_MOT_POS_DX PB6
-#define PIN2_MOT_POS_DX PB7
+#define PIN1_MOT_POS_DX PB3
+#define PIN2_MOT_POS_DX PB4
 
 #define PIN1_MOT_POS_SX PA8
 #define PIN2_MOT_POS_SX PA10
 
-#define PIN1_MOT_ANT_SX PA1
+#define PIN1_MOT_ANT_SX PA2
 #define PIN2_MOT_ANT_SX PA0
-
 
 #define PIN_I2C_SDA PB3
 #define PIN_I2C_SCL PB4
 
 
+#ifdef BLACKPILL_F411CE
+  #define PIN_WIRE_SDA PB7 //default in wire.h (se cambi valore warning di ridefinizione)
+  #define PIN_WIRE_SCL PB6 //default in wire.h (se cambi valore warning di ridefinizione)
+#endif 
 
 
 #endif // ROBOTPROPIN_H
---
+++ software/MainBlackPillCode/src/test/motori_avanti_indietro.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "robot.h"
+
+Robot robot;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {robot.test_motori();
+}
