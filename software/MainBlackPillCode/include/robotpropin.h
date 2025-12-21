ba279996a893d77cd1a403e31d3c690fb82208e9
paolo <paolo@foo.com>
2025-12-21 08:44:20 +0100

    aggiunge commenti con timer e channel ricavati da PeripheralPins.c ( variants in stm32duino)

--- software/MainBlackPillCode/include/robotpropin.h
+++ software/MainBlackPillCode/include/robotpropin.h
@@ -17,17 +17,17 @@
 
 #ifdef BLACKPILL_F411CE
 
-#define PIN1_MOT_ANT_DX PA2
-#define PIN2_MOT_ANT_DX PA3
+#define PIN1_MOT_ANT_DX PA2  //TIM2_CH3
+#define PIN2_MOT_ANT_DX PA3  //TIM2_CH4
 
-#define PIN1_MOT_POS_DX PA0
-#define PIN2_MOT_POS_DX PA1
+#define PIN1_MOT_POS_DX PA0  //TIM2_CH1
+#define PIN2_MOT_POS_DX PA1  //TIM2_CH2
 
-#define PIN1_MOT_POS_SX PB5
-#define PIN2_MOT_POS_SX PB8 
+#define PIN1_MOT_POS_SX PB5  //TIM3_CH2
+#define PIN2_MOT_POS_SX PB8  //TIM4_CH3
 
-#define PIN1_MOT_ANT_SX PA6
-#define PIN2_MOT_ANT_SX PA7
+#define PIN1_MOT_ANT_SX PA6  //TIM3_CH1
+#define PIN2_MOT_ANT_SX PA7  //TIM1_CH1N 
 
 
   #define PIN_WIRE_SDA PB7 //default in wire.h (se cambi valore warning di ridefinizione)
