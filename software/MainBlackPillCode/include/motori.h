104ffabe29c5dda8f12be17b3eabd278a5ac621b
paolo <paolo@foo.com>
2025-12-25 12:14:59 +0100

    modificata ISR per riflettere convenzioni : ANTI positiva , ORA negativa

--- software/MainBlackPillCode/include/motori.h
+++ software/MainBlackPillCode/include/motori.h
@@ -116,11 +116,11 @@ void Motore::ISR_encoder()
   // Determina la direzione usando il canale B
   if (digitalRead(_pin_enc2) == HIGH)
   {
-    encoderCount--;
+    encoderCount++;
   }
   else
   {
-    encoderCount++;
+    encoderCount--;
   }
 }
 
