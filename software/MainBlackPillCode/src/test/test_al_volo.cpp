4a6e768dec6ebfaf79eb73aaec020c830a8438ff
paolo <paolo@foo.com>
2026-01-18 19:12:39 +0100

    aggiunto contdown

--- software/MainBlackPillCode/src/test/test_al_volo.cpp
+++ software/MainBlackPillCode/src/test/test_al_volo.cpp
@@ -2,10 +2,23 @@
 
 void setup()
 {
+Serial.begin(115200);
 
+    { // starting serial e programma
+        Serial.begin(115200);
+        while (!Serial1)
+            ;
+        Serial.println("Starting  =>   test_controller_RPM_e_PID");
+
+        for (int i = 10; i > 0; i--)
+        {
+            Serial.print("inizio tra ");
+            Serial.println(i);
+            delay(200);
+        };
+    }
 }
 
-const unsigned long int delta_sample = 10 ;
 
 void loop()
 {
