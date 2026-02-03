3971344a67f8da345cd9f94e36a91ecd19fad230
paolo <paolo@foo.com>
2026-02-03 21:42:13 +0100

    aggiornate misure

--- software/MainBlackPillCode/src/test/misura_durata_funzioni.cpp
+++ software/MainBlackPillCode/src/test/misura_durata_funzioni.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include "controller.h"
-#include "bussola.h"
+//#include "bussola.h"
 
 //USARE CON   NO_PID NO_RPM NO_BUSSOLA (?)
 void setup()
@@ -9,7 +9,7 @@ void setup()
         Serial.begin(115200);
         while (!Serial1)
             ;
-        Serial.println("Starting  =>   test_controller_RPM_e_PID");
+        Serial.println("Starting  =>   misura durata funzioni");
 
         for (int i = 10; i > 0; i--)
         {
@@ -19,7 +19,11 @@ void setup()
         };
     }
     controller.init();
-    bussola.begin();
+    Serial.println("Starting  =>   misura durata funzioni");
+    //bussola.begin();
+        for ( int i=0;i<4;i++){
+        controller.motori[i].set_target_RPM(500);
+    }
 
 }
 
@@ -28,6 +32,7 @@ void setup()
 void loop()
 {
     delay(1000);
+
     ora_inizio=micros();
     controller.aggiorna_PID_dei_quattro_motori();
     ora_fine=micros();
@@ -47,7 +52,7 @@ void loop()
     Serial.println(" uSec \n");
 
     ora_inizio=micros();
-    bussola.update_heading();
+    //bussola.update_heading();
     ora_fine=micros();
 
     Serial.print(" Bussola update_heading() ");
@@ -56,5 +61,38 @@ void loop()
 
 
 
+    ora_inizio=micros();
+    controller._mot_ant_dx.ISR_encoder();
+    ora_fine=micros();
+
+    Serial.print(" motore.ISR_encoder()() ");
+    Serial.print(ora_fine -ora_inizio);
+    Serial.println(" uSec \n");
+
+    Serial.println(" \n\n\n");
+
 
 }
+/* con 4 motori accesi pid e rpm
+
+
+ controller aggiorna pid 4 motori () 97 uSec 
+
+ controller aggiorna RPM 4 motori()  7 uSec 
+
+ Bussola update_heading() 1 uSec 
+
+ motore.ISR_encoder()() 1 uSec 
+
+ 
+
+controller aggiorna pid 4 motori () 90 uSec 
+
+ controller aggiorna RPM 4 motori()  8 uSec 
+
+ Bussola update_heading() 0 uSec 
+
+ motore.ISR_encoder()() 2 uSec 
+
+
+*/
